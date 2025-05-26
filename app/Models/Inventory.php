50f441845079654856bdc5c878bbfa1b3a19de7d
Deep <deepbis1994@gmail.com>
2025-05-26 21:31:09 +0530

    release latest

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Inventory extends Model
 {
+    use SoftDeletes;
+    
     protected $table = 'inventory';
 
     protected $fillable = ["item_code","product_id","total_stock","buying_price"];
